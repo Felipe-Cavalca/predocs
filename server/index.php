d0e261477d4a81fd69ebd9ef0baa3c66d607f4c3
Felipe-Cavalca <felipe.cavalca@tmh.com.br>
2022-03-21 21:27:28 -0300

    Criado index do servidor para direcionar as funcoes
    Signed-off-by: Felipe-Cavalca <felipe.cavalca@tmh.com.br>

--- server/index.php
+++ server/index.php
@@ -11,47 +11,54 @@ include 'core/banco.php';
 $_POST = json_decode(file_get_contents("php://input"));
 
 //divite o pagina pela / para qu consiga controler o controller e a função
-$_GET['_Pagina'] = explode('/', $_GET['_Pagina']);
+if(isset($_GET['_Pagina'])){
+    $_GET['_Pagina'] = explode('/', $_GET['_Pagina']);
 
-//verifica se o arquivo existe
-if (isset($_GET['_Pagina']) &&  file_exists('controllers/' . $_GET['_Pagina'][0] . 'Controller.php')) {
+    //verifica se o arquivo existe
+    if (isset($_GET['_Pagina']) &&  file_exists('controllers/' . $_GET['_Pagina'][0] . 'Controller.php')) {
 
-    //inclui o arquivo
-    include 'controllers/' . $_GET['_Pagina'][0] . 'Controller.php';
+        //inclui o arquivo
+        include 'controllers/' . $_GET['_Pagina'][0] . 'Controller.php';
 
-    //verifica se a função existe
-    if (function_exists($_GET['_Pagina'][1])) {
-        //caso exista o parametro para ser enviado a função
-        if (isset($_GET['_Pagina'][2])) {
-            $retornoFuncao = $_GET['_Pagina'][1]($_GET['_Pagina'][2]);
-        } else {
-            $retornoFuncao = $_GET['_Pagina'][1]();
+        //verifica se a função existe
+        if (isset($_GET['_Pagina'][1]) && function_exists($_GET['_Pagina'][1])) {
+            //caso exista o parametro para ser enviado a função
+            if (isset($_GET['_Pagina'][2])) {
+                $retornoFuncao = $_GET['_Pagina'][1]($_GET['_Pagina'][2]);
+            } else {
+                $retornoFuncao = $_GET['_Pagina'][1]();
+            }
+
+            //verifica se o retorno é uma função
+            if (is_array($retornoFuncao)) {
+                $_Retorno['funcao'] = $retornoFuncao;
+            } else {
+                $_Retorno['funcao']['retorno'] = $retornoFuncao;
+            }
+
+            //define a resposta padrão de sucesso
+            $_Retorno['servidor'] = [
+                'stts' => true,
+                'msg' => 'Função executada com sucesso'
+            ];
         }
-
-        //verifica se o retorno é uma função
-        if (is_array($retornoFuncao)) {
-            $_Retorno['funcao'] = $retornoFuncao;
-        } else {
-            $_Retorno['funcao']['retorno'] = $retornoFuncao;
+        //caso a função não exista
+        else {
+            $_Retorno['servidor'] = [
+                'stts' => false,
+                'funcao' => 'Função não localizada no controller'
+            ];
         }
-
-        //define a resposta padrão de sucesso
+    } else {
         $_Retorno['servidor'] = [
-            'stts' => true,
-            'msg' => 'Função executada com sucesso'
+            'status' => false,
+            'msg' => 'Controller não encontrado'
         ];
     }
-    //caso a função não exista
-    else {
-        $_Retorno['servidor'] = [
-            'stts' => false,
-            'funcao' => 'Função não localizada no controller'
-        ];
-    }
-} else {
+}else{
     $_Retorno['servidor'] = [
         'status' => false,
-        'msg' => 'Controller não encontrado'
+        'msg' => 'Nenhum caminho definido'
     ];
 }
 
