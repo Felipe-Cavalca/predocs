6318390495bf86cc4f181a40b3a153ddd31173f5
Felipe-Cavalca <flpsnocvla@gmail.com>
2022-04-18 18:42:33 -0300

    Criação da classes de funções

---
+++ core/Funcoes.php
@@ -0,0 +1,34 @@
+<?php
+
+class Funcoes
+{
+	/**
+	 * Função para printar algo na tela
+	 *
+	 * @param any $data
+	 * @return string
+	 */
+	function pr($data)
+	{
+		echo '<pre>' . print_r($data, true) . '</pre>';
+	}
+
+	/**
+	 * Função para listar os arquivos de uma pasta
+	 *
+	 * @param string $path - caminho da lista de pastas
+	 * @return array - arry com os nomes dos arquivos/pastas de dentro do diretorio
+	 */
+	public function listar($path = '/')
+	{
+		$diretorio = dir($path);
+
+		$arquivos = [];
+		while ($arquivo = $diretorio->read()) {
+			$arquivos[] = $arquivo;
+		}
+		$diretorio->close();
+
+		return $arquivos;
+	}
+}
