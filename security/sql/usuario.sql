c3a52d44123335083e23bfe6b52d8ab6fe93cee9
Felipe-Cavalca <flpsnocvla@gmail.com>
2022-07-06 19:48:51 -0300

    corigindo sql default lis
    Signed-off-by: Felipe-Cavalca <flpsnocvla@gmail.com>

--- security/sql/usuario.sql
+++ security/sql/usuario.sql
@@ -1,10 +1,9 @@
 create table if not exists `usuario` (
-	`id` int(11) NOT NULL UNIQUE AUTO_INCREMENT,
+	`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`nome` varchar(255) NOT NULL,
 	`sobrenome` varchar(255),
 	`email` varchar(255) NOT NULL,
 	`ativo` boolean,
 	`created` datetime,
-	`modified` datetime,
-	PRIMARY KEY (`id`)
+	`modified` datetime
 );
