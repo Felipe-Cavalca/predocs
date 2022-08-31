670b9d3444c586969ea353f339be3a4e5d0c188c
Felipe Cavalca <flpsnocvla@gmail.com>
2022-08-31 19:52:59 -0300

    copiando dados de web para app
    Signed-off-by: Felipe Cavalca <flpsnocvla@gmail.com>

---
+++ app/components/js/nav.js
@@ -0,0 +1,1 @@
+initVueDefault("nav");
---
+++ app/js/global/funcoes.js
@@ -0,0 +1,15 @@
+/**
+ * Função para criação de uma pagina basica do vue js 3
+ * @param {domElement} e elemento dom
+ */
+function initVueDefault(e) {
+    const pagina = {
+        data() {
+            return {
+                VarsGlobal: VarsGlobal,
+            };
+        },
+    };
+
+    Vue.createApp(pagina).mount(e);
+}
