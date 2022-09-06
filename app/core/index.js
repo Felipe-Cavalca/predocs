dda8276532b2208743173785258a0d5caf34b1f6
Felipe Cavalca <flpsnocvla@gmail.com>
2022-09-06 19:32:33 -0300

    Mudando inportação do manifest.json
    Signed-off-by: Felipe Cavalca <flpsnocvla@gmail.com>

--- app/core/index.js
+++ app/core/index.js
@@ -89,7 +89,8 @@ try {
     function PWA() {
         var link = document.createElement("link");
         link.setAttribute("rel", "manifest");
-        link.setAttribute("href", "data:json;base64," + btoa(Lis.getConfig("manifest")));
+        link.setAttribute("href", Lis.getUrl("/model/manifest.json"));
+        // link.setAttribute("href", "data:json;base64," + btoa(Lis.getConfig("manifest")));
         document.querySelector("head").prepend(link);
 
         if ("serviceWorker" in navigator) {
