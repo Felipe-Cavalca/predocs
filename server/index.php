2117907e56c91450567e3cdf22a7ca8665c9bd9c
Felipe Cavalca <flpsnocvla@gmail.com>
2022-09-24 01:38:00 -0300

    Adicionando funções na classe funções
    Signed-off-by: Felipe Cavalca <flpsnocvla@gmail.com>

--- server/index.php
+++ server/index.php
@@ -19,25 +19,27 @@ init();
  */
 function init()
 {
-	configPHP(); //seta as config do php
-	post(); //organiza o $_POST
-	get(); //organiza o $_GET
+	$Funcoes = new funcoes();
+
+	$Funcoes->configPHP(); //seta as config do php
+	$Funcoes->post(); //organiza o $_POST
+	$Funcoes->get(); //organiza o $_GET
 
 	if (empty($_GET["controller"])) {
-		naoEncontrado();
+		$Funcoes->naoEncontrado();
 		return 0;
 	}
 
-	$controller = incluiController($_GET["controller"]);
+	$controller = $Funcoes->incluiController($_GET["controller"]);
 
 	if ($controller === false) {
-		naoEncontrado();
+		$Funcoes->naoEncontrado();
 		return 0;
 	} else if ($controller === true) {
 		if (function_exists($_GET["function"])) {
 			$returnFunction = call_user_func($_GET["function"], $_GET["param"]);
 		} else {
-			naoEncontrado();
+			$Funcoes->naoEncontrado();
 			return 0;
 		}
 	} else if (method_exists($controller, $_GET["function"])) {
