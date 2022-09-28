4cb1be15d0e52b6776475473d4eefa612f3325b1
Felipe Cavalca <flpsnocvla@gmail.com>
2022-09-28 00:01:05 -0300

    alterando maneira de validar as funções
    Signed-off-by: Felipe Cavalca <flpsnocvla@gmail.com>

--- server/core/Funcoes.php
+++ server/core/Funcoes.php
@@ -17,6 +17,38 @@ class funcoes
 	}
 
 	/**
+	 * funcao para analizar a request de um usuario
+	 * @return mixed
+	 */
+	public function init()
+	{
+		$this->configPHP(); //seta as config do php
+		$this->post(); //organiza o $_POST
+		$this->get(); //organiza o $_GET
+
+		if (empty($_GET["controller"])) return $this->naoEncontrado();
+
+		$controller = $this->incluiController($_GET["controller"]);
+
+		switch (gettype($controller)) {
+			case "integer":
+				if ($controller == 404) return $this->naoEncontrado();
+				if ($controller == 401) return $this->semAutorizacao();
+				if ($controller == 200 && function_exists($_GET["function"]))
+					return call_user_func($_GET["function"], $_GET["param"]);
+				else return $this->naoEncontrado();
+				break;
+			case "object":
+				if (method_exists($controller, $_GET["function"]))
+					return call_user_func([$controller, $_GET["function"]], $_GET["param"]);
+				else return $this->naoEncontrado();
+				break;
+		}
+
+		return $this->erroInterno();
+	}
+
+	/**
 	 * Função para listar os arquivos de uma pasta
 	 *
 	 * @param string $pasta - caminho da lista de pastas
@@ -56,7 +88,7 @@ class funcoes
 	 * @param array $array - array a ser verificado
 	 * @return ["status" => boolean, "msg" => string]
 	 */
-	public function emptyPost(array $indices = [], array $array = [])
+	public function empty(array $indices = [], array $array = [])
 	{
 		if (empty($array)) $array = $_POST;
 		foreach ($indices as $indice) {
@@ -83,7 +115,7 @@ class funcoes
 	 * Função para setar as config do php
 	 * @return void
 	 */
-	public function configPHP()
+	private function configPHP()
 	{
 		header("Access-Control-Allow-Origin: *");
 		header("Access-Control-Allow-Headers: *");
@@ -100,23 +132,26 @@ class funcoes
 		ini_set("memory_limit", "5G"); //pegar do config
 
 		ini_set("display_errors", $config->debug ? "1" : "0");
+
+		return;
 	}
 
 	/**
 	 * Função para validar se os dados estão vindo via json ou form-encode
 	 * @return void
 	 */
-	public function post()
+	private function post()
 	{
 		$json = json_decode(file_get_contents('php://input'), true);
 		$_POST = (is_array($json) ? $json : $_POST);
+		return;
 	}
 
 	/**
 	 * Função para organizar os dados do get
 	 * @return void
 	 */
-	public function get()
+	private function get()
 	{
 		$retorno = [];
 		$url = explode("/", isset($_GET["_Pagina"]) ? $_GET["_Pagina"] : "");
@@ -124,26 +159,37 @@ class funcoes
 		$retorno["function"] = empty($url[1]) ? "index" : $url[1];
 		$retorno["param"] = isset($url[2]) ? $url[2] : null;
 		$_GET = $retorno;
+		return;
 	}
 
 	/**
 	 * Retorna para quem está fazendo a solicitação o erro 404
-	 * @return void
+	 * @return array
 	 */
 	public function naoEncontrado()
 	{
 		http_response_code(404);
-		echo json_encode(["status" => false, "msg" => "A função solicitada não foi encontrada"]);
+		return ["status" => false, "msg" => "A função solicitada não foi encontrada"];
 	}
 
 	/**
 	 * Chamado para paginas em que o usuario não tem permissão para acessar
-	 * @return void
+	 * @return array
 	 */
 	public function semAutorizacao()
 	{
 		http_response_code(401);
-		echo json_encode(["status" => false, "msg" => "Acesso negado"]);
+		return ["status" => false, "msg" => "Acesso negado"];
+	}
+
+	/**
+	 * Função caso haja um erro interno no sistema (500)
+	 * @return array
+	 */
+	public function erroInterno()
+	{
+		http_response_code(500);
+		return ["status" => false, "msg" => "Erro interno"];
 	}
 
 	/**
@@ -151,33 +197,25 @@ class funcoes
 	 * caso haja uma classe, retorna o mesmo
 	 * caso não haja - true e inclui o arquivo
 	 * @param string $nome - nome do controller
-	 * @return obj|boolean|null
+	 * @return object|int obj para o controller, int com o status da importação
 	 */
 	public function incluiController(string $nomeController)
 	{
 		$config = new Config;
 		$controller = new Arquivo("{$config->getCaminho("controller")}/{$nomeController}Controller.php");
 
+		//valida se o controller não é uma função do framework
 		switch ($nomeController) {
 			case "autorun":
 				$controller = new Arquivo("{$config->getCaminho("functions")}/autorun.php");
-			default:
-				if ($controller->existe()) {
-					$controller->renderiza();
-					if (class_exists($nomeController)) {
-						$obj = new $nomeController();
-						if (method_exists($obj, "__autorizado") && $obj->__autorizado() === false) {
-							$this->semAutorizacao();
-							return null;
-						} else {
-							return $obj;
-						}
-					} else {
-						return true;
-					}
-				} else {
-					return false;
-				}
+				break;
 		}
+
+		if (!$controller->existe()) return 404;
+		$controller->renderiza();
+		if (!class_exists($nomeController)) return 200;
+		$obj = new $nomeController();
+		if (method_exists($obj, "__autorizado") && $obj->__autorizado() === false) return 401;
+		return $obj;
 	}
 }
