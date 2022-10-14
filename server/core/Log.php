1468024e2e9d524dcea5d498ad926c2065426c00
Felipe Cavalca <flpsnocvla@gmail.com>
2022-10-13 22:45:23 -0300

    refatorando arquivo de log
    Signed-off-by: Felipe Cavalca <flpsnocvla@gmail.com>

--- server/core/Log.php
+++ server/core/Log.php
@@ -6,17 +6,54 @@
 class Log extends Arquivo
 {
     /**
-     * Função construtora do arquivo no storage
-     *
-     * @param string - Mensagem
+     * Função para escrever algo no storage
+     * @version 1
+     * @access public
+     * @param string $mensagem mensagem que será adicionada no log
      */
-    public function __construct(string $mensagem)
+    public function __construct(string $mensagem = "")
+    {
+        if (empty($mensagem))
+            return;
+
+        $this->escreveLog($mensagem);
+
+        return;
+    }
+
+    /**
+     * Função para escrever mensagem de erro no log
+     * @version 1
+     * @access public
+     * @param string $mensagem mensagem de erro
+     * @param string $controller nome do controller
+     * @param string $function funcao que acusou erro
+     */
+    public function erroLog(string $msg, string $controller = "", string $function = ""): void
+    {
+        if (empty($controller))
+            $controller = $_GET["controller"];
+
+        if (empty($function))
+            $function = $_GET["function"];
+
+        $this->escreveLog("Erro em {$controller} - {$function} : $msg");
+        return;
+    }
+
+    /**
+     * Função para criar e escrever no arquivo de log
+     * @version 1
+     * @access private
+     * @param string $mensagem Mensagem que será escrita no log
+     */
+    private function escreveLog(string $mensagem)
     {
         $config = new Config();
         $dia = date("Y/m/d");
         $hora = date("H:i:s");
         parent::__construct("{$config->getCaminho("log")}/{$dia}.log");
-        $this->criar();
-        $this->adicionar("{$dia} {$hora} - {$mensagem}\r\n");
+        parent::criar();
+        parent::adicionar("{$dia} {$hora} - {$mensagem}\r\n");
     }
 }
