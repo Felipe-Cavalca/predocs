1cddc3fa03defccc6c5b5fdb8ff0f21dbe974df3
Felipe Cavalca <flpsnocvla@gmail.com>
2022-10-16 00:16:58 -0300

    refatorando funções
    Signed-off-by: Felipe Cavalca <flpsnocvla@gmail.com>

--- server/core/Funcoes.php
+++ server/core/Funcoes.php
@@ -5,22 +5,14 @@
  */
 class funcoes
 {
-	/**
-	 * Função para printar algo na tela
-	 *
-	 * @param mixed $data
-	 * @return string
-	 */
-	public function pr(mixed $data)
-	{
-		echo '<pre>' . print_r($data, true) . '</pre>';
-	}
 
 	/**
 	 * funcao para analizar a request de um usuario
+	 * @version 1
+	 * @access public
 	 * @return mixed
 	 */
-	public function init()
+	public function init(): mixed
 	{
 		$this->configPHP(); //seta as config do php
 		$this->post(); //organiza o $_POST
@@ -50,11 +42,12 @@ class funcoes
 
 	/**
 	 * Função para listar os arquivos de uma pasta
-	 *
-	 * @param string $pasta - caminho da lista de pastas
-	 * @return array - array com os nomes dos arquivos/pastas de dentro do diretorio
+	 * @version 1
+	 * @access public
+	 * @param string $pasta caminho da lista de pastas
+	 * @return array array com os nomes dos arquivos/pastas de dentro do diretorio
 	 */
-	public function listarArquivos(string $pasta = '/')
+	public function listarArquivos(string $pasta = '/'): array
 	{
 		$diretorio = dir($pasta);
 		$arquivos = [];
@@ -67,11 +60,13 @@ class funcoes
 
 	/**
 	 * Valida se campos existem em um array
-	 * @param array $indices - nome dos campos a serem verificados
-	 * @param array $array - array a ser verificado
+	 * @version 1
+	 * @access public
+	 * @param array $indices nome dos campos a serem verificados
+	 * @param array $array array a ser verificado
 	 * @return array ["status" => boolean, "msg" => string]
 	 */
-	public function isset(array $indices = [], array $array = [])
+	public function isset(array $indices = [], array $array = []): array
 	{
 		if (empty($array)) $array = $_POST;
 		foreach ($indices as $indice) {
@@ -84,11 +79,13 @@ class funcoes
 
 	/**
 	 * Valida se indices de um array não estão vazios
-	 * @param array $indices - nome dos indices a serem verificados
-	 * @param array $array - array a ser verificado
+	 * @version 1
+	 * @access public
+	 * @param array $indices nome dos indices a serem verificados
+	 * @param array $array array a ser verificado
 	 * @return ["status" => boolean, "msg" => string]
 	 */
-	public function empty(array $indices = [], array $array = [])
+	public function empty(array $indices = [], array $array = []): array
 	{
 		if (empty($array)) $array = $_POST;
 		foreach ($indices as $indice) {
@@ -102,20 +99,24 @@ class funcoes
 
 	/**
 	 * Valida se uma pasta existe, caso não exista cria a mesma
+	 * @version 1
+	 * @access public
 	 * @param string $caminho - caminho até a pasta
 	 * @param int $permission - permissão da pasta
 	 * @return bool
 	 */
-	public function criaPasta(string $path, int $permission = 0777)
+	public function criaPasta(string $path, int $permission = 0777): bool
 	{
 		if (!is_dir($path)) return mkdir($path, $permission, true);
 	}
 
 	/**
 	 * Função para setar as config do php
+	 * @version 1
+	 * @access public
 	 * @return void
 	 */
-	private function configPHP()
+	private function configPHP(): void
 	{
 		header("Access-Control-Allow-Origin: *");
 		header("Access-Control-Allow-Headers: *");
@@ -131,16 +132,18 @@ class funcoes
 
 		ini_set("memory_limit", "5G"); //pegar do config
 
-		ini_set("display_errors", $config->debug ? "1" : "0");
+		ini_set("display_errors", "1");
 
 		return;
 	}
 
 	/**
 	 * Função para validar se os dados estão vindo via json ou form-encode
+	 * @version 1
+	 * @access public
 	 * @return void
 	 */
-	private function post()
+	private function post(): void
 	{
 		$json = json_decode(file_get_contents('php://input'), true);
 		$_POST = (is_array($json) ? $json : $_POST);
@@ -149,9 +152,11 @@ class funcoes
 
 	/**
 	 * Função para organizar os dados do get
+	 * @version 1
+	 * @access public
 	 * @return void
 	 */
-	private function get()
+	private function get(): void
 	{
 		$retorno = [];
 		$url = explode("/", isset($_GET["_Pagina"]) ? $_GET["_Pagina"] : "");
@@ -164,9 +169,11 @@ class funcoes
 
 	/**
 	 * Retorna para quem está fazendo a solicitação o erro 404
+	 * @version 1
+	 * @access public
 	 * @return array
 	 */
-	public function naoEncontrado()
+	public function naoEncontrado(): array
 	{
 		http_response_code(404);
 		return ["status" => false, "msg" => "A função solicitada não foi encontrada"];
@@ -174,9 +181,11 @@ class funcoes
 
 	/**
 	 * Chamado para paginas em que o usuario não tem permissão para acessar
+	 * @version 1
+	 * @access public
 	 * @return array
 	 */
-	public function semAutorizacao()
+	public function semAutorizacao(): array
 	{
 		http_response_code(401);
 		return ["status" => false, "msg" => "Acesso negado"];
@@ -184,9 +193,11 @@ class funcoes
 
 	/**
 	 * Função caso haja um erro interno no sistema (500)
+	 * @version 1
+	 * @access public
 	 * @return array
 	 */
-	public function erroInterno()
+	public function erroInterno(): array
 	{
 		http_response_code(500);
 		return ["status" => false, "msg" => "Erro interno"];
@@ -194,12 +205,14 @@ class funcoes
 
 	/**
 	 * inclui um controller
+	 * @version 1
+	 * @access public
+	 * @param string $nome nome do controller
+	 * @return object|int obj para o controller, int com o status da importação
 	 * caso haja uma classe, retorna o mesmo
 	 * caso não haja - true e inclui o arquivo
-	 * @param string $nome - nome do controller
-	 * @return object|int obj para o controller, int com o status da importação
 	 */
-	public function incluiController(string $nomeController)
+	public function incluiController(string $nomeController): mixed
 	{
 		$config = new Config;
 		$controller = new Arquivo("{$config->getCaminho("controller")}/{$nomeController}Controller.php");
@@ -218,4 +231,16 @@ class funcoes
 		if (method_exists($obj, "__autorizado") && $obj->__autorizado($_GET["function"]) === false) return 401;
 		return $obj;
 	}
+}
+
+/**
+ * Função para printar algo na tela
+ * @version 1
+ * @access public
+ * @param mixed $data
+ * @return void
+ */
+function pr(mixed $data): void
+{
+	echo '<pre>' . print_r($data, true) . '</pre>';
 }
