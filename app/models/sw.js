3a59133613ee64cff225d3abd2a1ec04f49c125a
Felipe Cavalca <flpsnoccvla@gmail.com>
2023-10-21 22:05:04 -0300

    Melhorando serviçe worker

--- app/models/sw.js
+++ app/models/sw.js
@@ -1,25 +1,42 @@
-var CACHE_NAME = "Cache-App";
+const CACHE_NAME = "Cache-App-v2"; // Atualize o nome do cache para indicar uma nova versão
 
 self.addEventListener("install", event => {
-    event.waitUntil(caches.open(CACHE_NAME).then(cache => {
-        var urlsCACHE = "___ARRAY_DE_ARQUIVOS_AQUI___";
-
-        return cache.addAll(urlsCACHE);
-    }))
-});
-
-self.addEventListener("fetch", event => {
-    event.respondWith(caches.match(event.request).then(cachedResponse => { return cachedResponse || fetch(event.request); }));
-});
-
-self.addEventListener("activate", function activator(event) {
     event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(keys
-                .filter(key => { return key.indexOf(CACHE_NAME) !== 0; })
-                .map(key => { return caches.delete(key); })
-            );
+        caches.open(CACHE_NAME).then(cache => {
+            // Coloque os URLs dos recursos que deseja armazenar em cache
+            const urlsToCache = [
+                "/",
+            ];
+
+            return cache.addAll(urlsToCache);
         })
     );
 });
 
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            return cachedResponse || fetch(event.request).then(networkResponse => {
+                const clonedResponse = networkResponse.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, clonedResponse);
+                });
+                return networkResponse;
+            }).catch(error => {
+                console.error("Erro ao buscar recurso:", error);
+                // Você pode lidar com erros de busca aqui, se necessário.
+            });
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+});
