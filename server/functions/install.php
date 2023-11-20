5babc05df8d4ce17179d06777750824192e261ea
Felipe Cavalca <flpsnoccvla@gmail.com>
2023-11-20 19:39:22 -0300

    Refatorando instalador

--- server/functions/install.php
+++ server/functions/install.php
@@ -1,17 +1,13 @@
 <?php
 
-class install
+class Install
 {
-    //veriavel q guarda a url até o sistema
     public $url;
-
-    //variavel q guarda o caminho apartir de / na url
     public $caminho;
 
     /**
-     * Função contrutora do objeto install
-     * @version 2
-     * @access public
+     * Função construtora da instância da classe.
+     * Inicializa os caminhos e URLs apropriados.
      */
     public function __construct()
     {
@@ -27,47 +23,117 @@ class install
 
     /**
      * Função para realizar a instalação do framework em uma pasta
-     * @version 2.1.0
-     * @access public
      * @return string mensagem
      */
-    public function index($cache = "false"): string
+    public function index(): string
     {
-        $etapas = [];
+        Log::registrar("iniciando Instalação");
 
-        $etapas["copiaConfigApp"] = function () {
-            $config = new Config;
-            $funcoes = new funcoes;
-            $caminhoOriginal = "{$config->getCaminho("app")}/models/config/";
-            $caminhoConfig = "{$config->getCaminho("app")}/config/";
-            foreach ($funcoes->listarArquivos($caminhoOriginal) as $nome) {
-                if ($nome == "." || $nome == "..")
+        $this->copiaConfigApp();
+        $this->editaConfigApp();
+        $this->editaManifest();
+        $this->instalaBanco();
+
+        Log::registrar("Instalação Finalizada");
+
+        return "Todas as etapas foram executadas, para mais informações veja o log";
+    }
+
+    /**
+     * Copia os arquivos de configuração do diretório de modelo para o diretório de configuração do aplicativo.
+     *
+     * Esta função verifica e copia cada arquivo de configuração do diretório original
+     * para o diretório de configuração do aplicativo, mantendo a estrutura de arquivos.
+     *
+     * @throws Exception Se ocorrer um erro durante o processo de cópia.
+     */
+    private function copiaConfigApp(): void
+    {
+        try {
+            Log::registrar("Copiando config do app");
+            // Define os caminhos para os diretórios originais e de destino
+            $caminhoOriginal = Config::getCaminho("app") . "/models/config/";
+            $caminhoConfig = Config::getCaminho("app") . "/config/";
+
+            // Obtém a lista de arquivos do diretório original
+            $arquivosOriginais = Funcoes::listarArquivos($caminhoOriginal);
+
+            // Verifica e copia cada arquivo para o diretório de configuração
+            foreach ($arquivosOriginais as $nomeArquivo) {
+                // Ignora os diretórios especiais '.' e '..'
+                if ($nomeArquivo === "." || $nomeArquivo === "..") {
                     continue;
+                }
 
-                $arquivo = new Arquivo($caminhoOriginal . $nome);
-                $arquivo->copiar($caminhoConfig . $nome);
+                // Cria instâncias de Arquivo para origem e destino
+                $arquivoOrigem = new Arquivo($caminhoOriginal . $nomeArquivo);
+
+                // Copia o arquivo de origem para o diretório de configuração
+                $arquivoOrigem->copiar($caminhoConfig . $nomeArquivo);
             }
-        };
+        } catch (Exception $e) {
+            Log::registrar("Erro ao copiar arquivos de configuração: " . $e->getMessage(), "Install", "copiaConfig");
+        }
+    }
 
-        $etapas["editaApp"] = function () {
-            $config = new config;
+    /**
+     * Edita o arquivo de configuração do aplicativo.
+     *
+     * Esta função realiza a edição do arquivo de configuração do aplicativo,
+     * adicionando novas variáveis ou atualizando as existentes com valores específicos.
+     *
+     * @throws Exception Se ocorrer um erro durante o processo de edição do arquivo.
+     */
+    private function editaConfigApp(): void
+    {
+        try {
+            Log::registrar("Editando configurações do app");
+            // Define os caminhos para o arquivo original e de destino
+            $caminhoOriginal = Config::getCaminho("app") . "/models/config/app.json";
+            $caminhoConfig = Config::getCaminho("app") . "/config/app.json";
 
-            $original = new Arquivo("{$config->getCaminho("app")}/models/config/app.json");
+            // Cria instâncias de Arquivo para origem e destino com permissão de escrita
+            $arquivoOriginal = new Arquivo($caminhoOriginal);
+            $arquivoConfig = new Arquivo($caminhoConfig, true);
 
-            $config = new Arquivo("{$config->getCaminho("app")}/config/app.json", true);
+            // Define as novas variáveis a serem adicionadas ou atualizadas
+            $novasVariaveis = [
+                "server" => $this->url . "server/",
+                "caminho" => $this->caminho
+            ];
 
-            $vars = ["server" => $this->url . "server/", "caminho" => $this->caminho];
+            // Lê o conteúdo do arquivo original e mescla com as novas variáveis
+            $conteudoOriginal = $arquivoOriginal->ler();
+            $novoConteudo = array_merge($conteudoOriginal, $novasVariaveis);
 
-            $config->escrever(array_merge($original->ler(), $vars));
-        };
+            // Escreve o novo conteúdo no arquivo de configuração do aplicativo
+            $arquivoConfig->escrever($novoConteudo);
+        } catch (Exception $e) {
+            Log::registrar("Erro ao editar o arquivo de configuração: " . $e->getMessage(), "Install", "editaConfigApp");
+        }
+    }
 
-        $etapas["editaManifest"] = function () {
-            $config = new config;
+    /**
+     * Edita o arquivo de manifesto do aplicativo.
+     *
+     * Esta função realiza a edição do arquivo de manifesto do aplicativo,
+     * inserindo ou atualizando valores específicos como a URL de início e atalhos.
+     *
+     * @throws Exception Se ocorrer um erro durante o processo de edição do arquivo.
+     */
+    private function editaManifest(): void
+    {
+        try {
+            Log::registrar("Editando manifesto");
+            // Define os caminhos para o arquivo original e de destino
+            $caminhoOriginal = Config::getCaminho("app") . "/models/config/manifest.json";
+            $caminhoConfig = Config::getCaminho("app") . "/config/manifest.json";
 
-            $original = new Arquivo("{$config->getCaminho("app")}/models/config/manifest.json");
+            // Cria instâncias de Arquivo para origem e destino com permissão de escrita
+            $arquivoOriginal = new Arquivo($caminhoOriginal);
+            $arquivoConfig = new Arquivo($caminhoConfig, true);
 
-            $config = new Arquivo("{$config->getCaminho("app")}/config/manifest.json", true);
-
+            // Define as novas configurações a serem adicionadas ou atualizadas
             $novasConfig = [
                 "start_url" => $this->url . "app/pages/",
                 "shortcuts" => [
@@ -78,40 +144,36 @@ class install
                 ]
             ];
 
-            $config->escrever(array_merge($original->ler(), $novasConfig));
-        };
+            // Lê o conteúdo do arquivo original e mescla com as novas configurações
+            $conteudoOriginal = $arquivoOriginal->ler();
+            $novoConteudo = array_merge($conteudoOriginal, $novasConfig);
 
-        $etapas["instalaBanco"] = function () {
-            $funcoes = new funcoes;
-
-            $autoRun = $funcoes->incluiController("autorun", true);
-            $autoRun->installBanco();
-        };
-
-        $etapas["listaArquivosApp"] = function ($cache) {
-            $config = new config;
-            $funcoes = new funcoes;
-
-            $caminho = $config->getCaminho("app");
-
-            $arquivo = new Arquivo("{$caminho}/sw.js", true);
-            $modelo = new Arquivo("{$caminho}/models/sw.js");
-
-            if ($cache == "true") {
-                $arquivos = $funcoes->listarArquivosRecursivos($caminho);
-                $arquivos = str_replace("./../", $this->caminho, $arquivos);
-                $arquivos = json_encode($arquivos, true);
-
-                $arquivo->escrever(str_replace('"___ARRAY_DE_ARQUIVOS_AQUI___"', $arquivos, $modelo->ler()));
-            } else {
-                $arquivo->escrever(str_replace('"___ARRAY_DE_ARQUIVOS_AQUI___"', "[]", $modelo->ler()));
-            }
-        };
-
-        foreach ($etapas as $etapa) {
-            $etapa($cache);
+            // Escreve o novo conteúdo no arquivo de manifesto do aplicativo
+            $arquivoConfig->escrever($novoConteudo);
+        } catch (Exception $e) {
+            Log::registrar("Erro ao editar o arquivo de manifesto: " . $e->getMessage(), "Install", "editaManifest");
         }
+    }
 
-        return "Todas as etapas foram executadas, para mais informações veja o log";
+    /**
+     * Instalação do banco de dados.
+     *
+     * Esta função realiza a instalação do banco de dados por meio do controlador "autorun",
+     * chamando o método "installBanco()" para executar o processo de instalação do banco de dados.
+     *
+     * @throws Exception Se ocorrer um erro durante a instalação do banco de dados.
+     */
+    private function instalaBanco(): void
+    {
+        try {
+            Log::registrar("Instalando banco");
+            // Inclui o controlador "autorun" para execução da instalação do banco de dados
+            $autoRun = Funcoes::incluiController("autorun", true);
+
+            // Chama o método "installBanco()" no controlador "autorun" para instalar o banco de dados
+            $autoRun->installBanco();
+        } catch (Exception $e) {
+            Log::registrar("Erro durante a instalação do banco de dados: " . $e->getMessage(), "Install", "instalaBanco");
+        }
     }
 }
