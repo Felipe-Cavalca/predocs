daee05ca57b1a36fef46a67e199f470245fa6524
Felipe Cavalca <flpsnoccvla@gmail.com>
2023-11-20 17:09:24 -0300

    Refatorando index

--- server/index.php
+++ server/index.php
@@ -1,51 +1,52 @@
 <?php
 
-function incluirArquivosDoCore()
+class IndexPreDocs
 {
-    include_once("./core/Funcoes.php");
-    include_once("./core/Arquivo.php");
-    include_once("./core/Config.php");
-    include_once("./core/Banco.php");
-    include_once("./core/Storage.php");
-    include_once("./core/Log.php");
-    include_once("./core/FuncoesApp.php");
-}
-
-function executarFramework()
-{
-    incluirArquivosDoCore();
-
-    // Crie instâncias dos objetos necessários
-    $banco = new Banco();
-    $funcoes = new Funcoes();
-
-    // Inicie o banco
-    $banco->inicio();
-
-    // Executa a função init()
-    $retorno = $funcoes->init() ?? $funcoes->setStatusCode(204);
-
-    // Trate o retorno
-    if (is_array($retorno)) {
-        echo json_encode($retorno);
-    } elseif (is_integer($retorno) || is_string($retorno)) {
-        echo json_encode(["retorno" => $retorno]);
-    } elseif (is_bool($retorno)) {
-        echo json_encode(["status" => $retorno]);
-    } else {
-        echo $retorno;
+    public static function incluirArquivosDoCore()
+    {
+        include_once("./core/Funcoes.php");
+        include_once("./core/Arquivo.php");
+        include_once("./core/Config.php");
+        include_once("./core/Banco.php");
+        include_once("./core/Storage.php");
+        include_once("./core/Log.php");
+        include_once("./core/FuncoesApp.php");
     }
 
-    // Verifique a condição e reverta ou salve o banco
-    if (isset($_SERVER["HTTP_TEST"])) {
-        if (http_response_code() == 200) {
-            $funcoes->setStatusCode(202);
+    public static function executarFramework()
+    {
+        static::incluirArquivosDoCore();
+
+        // Crie instâncias dos objetos necessários
+        $banco = new Banco();
+
+        // Inicie o banco
+        $banco->inicio();
+
+        // Executa a função init()
+        $retorno = Funcoes::init() ?? Funcoes::setStatusCode(204);
+
+        // Trate o retorno
+        if (is_array($retorno)) {
+            print json_encode($retorno);
+        } elseif (is_int($retorno) || is_string($retorno)) {
+            print json_encode(["retorno" => $retorno]);
+        } elseif (is_bool($retorno)) {
+            print json_encode(["status" => $retorno]);
+        } else {
+            print $retorno;
         }
-        $banco->reverter();
-    } else {
-        $banco->salvar();
+
+        // Verifique a condição e reverta ou salve o banco
+        if (isset($_SERVER["HTTP_TEST"])) {
+            if (http_response_code() == 200) {
+                Funcoes::setStatusCode(202);
+            }
+            $banco->reverter();
+        } else {
+            $banco->salvar();
+        }
     }
 }
 
-// Chame a função para executar o código
-executarFramework();
+IndexPreDocs::executarFramework();
