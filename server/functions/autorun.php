39330504f087fc307791a704f2b8eabdf924088f
Felipe Cavalca <flpsnoccvla@gmail.com>
2023-11-26 16:23:50 -0300

    refatorando aurotun

--- server/functions/autorun.php
+++ server/functions/autorun.php
@@ -7,124 +7,119 @@
 /**
  * Classe para a execução do auto run
  */
-class autorun
+class Autorun
 {
-    public $banco;
-    public $config;
 
     /**
      * Classe para inicializar as variaveis sempre que o objeto é criado
      */
     public function __construct()
     {
-        $this->banco = new Banco();
-        $this->config = new Config();
     }
 
     /**
-     * Função responsavel por instalar a base de dados
-     * @version 1
-     * @access public
+     * Executa scripts sql para criação do banco
+     *
      * @return bool
      */
     public function installBanco(): bool
     {
-        $configBanco = $this->config->getConfigBanco();
+        $configBanco = Config::getConfigBanco();
 
         if (!$configBanco["instalado"]) {
-            if (!$this->executaSqlPasta("{$this->config->getCaminho("sql")}/database/")) {
-                new Log("Erro ao criar tabelas");
+            if (!$this->executaSqlPasta(Config::getCaminho("sql") . "/database/")) {
+                Log::registrar("Erro ao criar tabelas");
                 return false;
             }
 
-            if (!$this->executaSqlPasta("{$this->config->getCaminho("sql")}/data/")) {
-                new Log("Erro ao inserir dados na base de dados");
+            if (!$this->executaSqlPasta(Config::getCaminho("sql") . "/data/")) {
+                Log::registrar("Erro ao inserir dados na base de dados");
                 return false;
             }
 
-            if ($configBanco["tipo"] == "mysql") {
-                if (!$this->executaSqlPasta("{$this->config->getCaminho("sql")}/triggers/")) {
-                    new Log("Erro ao adicionar triggers na base de dados");
-                    return false;
-                }
+            if ($configBanco["tipo"] == "mysql" && !$this->executaSqlPasta(Config::getCaminho("sql") . "/triggers/")) {
+                Log::registrar("Erro ao adicionar triggers na base de dados");
+                return false;
             }
 
-            $this->config->setConfigBanco(["instalado" => true]);
+            Config::setConfigBanco(["instalado" => true]);
         }
 
-        new Log("Banco instalado com sucesso");
+        Log::registrar("Banco instalado com sucesso");
         return true;
     }
 
     /**
      * Roda scripts para realizar a atulização do banco
-     * @version 1
-     * @access public
+     *
      * @return bool
      */
-    function updateBanco(): bool
+    public function updateBanco(): bool
     {
-        if (!$this->executaSqlPasta("{$this->config->getCaminho("sql")}/update/", true)) {
-            new Log("Erro ao atualizar banco de dados");
+        if (!$this->executaSqlPasta(Config::getCaminho("sql") . "/update/", true)) {
+            Log::registrar("Erro ao atualizar banco de dados");
             return false;
         }
 
-        new Log("Base de dados atualizada com sucesso");
+        Log::registrar("Base de dados atualizada com sucesso");
         return true;
     }
 
     /**
      * Função para apagar todas as tabelas da base de dados APAGAR EM PRODUÇÂO - APENAS PARA DEV
-     * @version 1
-     * @access public
+     *
      * @return bool
      */
     public function deleteBanco(): bool
     {
-        if ($this->banco->tipo == "mysql") {
-            foreach ($this->banco->query("show tables") as $tabela) {
-                if (!$this->banco->query("DROP TABLE {$tabela};")) {
-                    new Log("Erro ao excluir a tabela {$tabela} da base de dados");
+        $banco = new Banco();
+
+        if ($banco->tipo == "mysql") {
+            foreach ($banco->query("show tables") as $tabela) {
+                if (!$banco->query("DROP TABLE {$tabela};")) {
+                    Log::registrar("Erro ao excluir a tabela {$tabela} da base de dados");
                     return false;
                 }
             }
         } else {
-            $arquivo = new Arquivo(explode(":", $this->config->getConfigBanco()["stringConn"])[1]);
+            $arquivo = new Arquivo(explode(":", Config::getConfigBanco()["stringConn"])[1]);
             if (!$arquivo->apagar()) {
-                new Log("Erro ao excluir arquivo da base de dados");
+                Log::registrar("Erro ao excluir arquivo da base de dados");
                 return false;
             }
         }
 
-        $this->config->setConfigBanco(["instalado" => false]);
-        new Log("Base de dados apagada");
+        Config::setConfigBanco(["instalado" => false]);
+        Log::registrar("Base de dados apagada");
         return true;
     }
 
     /**
      * Executa arquivos sql de uma pasta
-     * @version 1
-     * @access public
+     *
      * @param string $pasta caminho para a pasta dos arquivos a serem executados
      * @return bool
      */
-    function executaSqlPasta($pasta): bool
+    public function executaSqlPasta($pasta): bool
     {
-        $funcoes = new funcoes();
-
-        foreach ($funcoes->listarArquivos($pasta) as $sql) {
+        $banco = new Banco();
+        foreach (Funcoes::listarArquivos($pasta) as $sql) {
 
             $script = new Arquivo($pasta . $sql);
 
-            if ($script->getExt() != "sql") continue;
+            if ($script->getExt() != "sql") {
+                continue;
+            }
 
             foreach (explode(";", $script->ler()) as $query) {
-                if (empty($this->removeCaracteres($query))) continue;
+                if (empty($this->removeCaracteres($query))) {
+                    continue;
+                }
 
-                $retorno = $this->banco->query("{$query};");
+                $retorno = $banco->query("{$query};");
 
                 if (!$retorno) {
-                    new Log("Erro ao executar o script {$pasta}{$sql}");
+                    Log::registrar("Erro ao executar o script {$pasta}{$sql}");
                     return false;
                 }
             }
@@ -135,10 +130,11 @@ class autorun
 
     /**
      * Remove caracteres de uma string
+     *
      * @param string $str string com dados a serem removidos
      * @return string
      */
-    function removeCaracteres($str)
+    public function removeCaracteres($str)
     {
         //variavel precisa estar assim pois é para pegar o quebra linha do arquivo .sql (não achei outra forma)
         $enter = "
