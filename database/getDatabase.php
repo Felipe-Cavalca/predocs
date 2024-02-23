97f887ea7af8e192310b9bb30ffcc1edfce8c1c3
Felipe Cavalca <flpsnocvla@gmail.com>
2024-02-23 16:59:54 -0300

    Criando função de instalação e atualização do banco de dados

--- database/getDatabase.php
+++ database/getDatabase.php
@@ -9,5 +9,15 @@ include_once __DIR__ . "/../server/Core/Settings.php";
 // Importa as classes necessárias
 use Predocs\Core\Database;
 
-// Inicializa a conexão com o banco de dados
-$database = new Database();
+// Função para executar SQLs
+function runSql($structurePath) {
+    $database = new Database();
+    $files = scandir($structurePath);
+
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..') {
+            $sql = file_get_contents($structurePath . '/' . $file);
+            $database->run($sql);
+        }
+    }
+}
