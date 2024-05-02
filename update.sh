65f7dc78fa1df14e2004498a8f50f7c6cc1bea7c
GUTO-GO <117954887+GUTO-GO@users.noreply.github.com>
2024-05-02 10:43:20 -0300

    Update update.sh
    Adicionando verificação de dependencisas no update.sh
    
    Signed-off-by: GUTO-GO <117954887+GUTO-GO@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Instalar o Docker e o Docker Compose se eles não estiverem instalados
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Docker não está instalado. Instalando...'
+  sudo apt-get install docker.io -y
+fi
+
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Docker Compose não está instalado. Instalando...'
+  sudo apt-get install docker-compose -y
+fi
+
 # Parar os serviços
 docker-compose down
 
