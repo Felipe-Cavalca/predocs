48a590634bdac1160d4e45f913800d21d53cab61
Felipe Cavalca <flpsnocvla@gmail.com>
2024-05-03 00:19:23 -0300

    instalando banco de dados ao executar scripts

--- scripts/update/execute_scripts.sh
+++ scripts/update/execute_scripts.sh
@@ -3,5 +3,8 @@
 # Instala dependencias
 docker-compose exec predocs bash scripts/install_dependencies.sh
 
+# Instala o banco de dados
+docker-compose exec predocs bash scripts/install_db.sh
+
 # Atualiza o banco de dados
 docker-compose exec predocs bash scripts/update_db.sh
