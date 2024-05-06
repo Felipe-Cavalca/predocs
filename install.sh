2f0b80bb25b56a2ae5dee174580cdeac2f0f391e
Felipe Cavalca <flpsnocvla@gmail.com>
2024-05-06 13:05:06 -0300

    Corrigindo instalador

--- install.sh
+++ install.sh
@@ -6,11 +6,31 @@ repo_url="https://github.com/Felipe-Cavalca/predocs.git"
 # Extrair o nome do repositório da URL do Git
 repo_name=$(basename "$repo_url" .git)
 
-# Executa os scripts
-source ./scripts/update/verify_root.sh
-source ./scripts/update/update_sistem.sh
-source ./scripts/update/verify_docker.sh
-source ./scripts/update/verify_git.sh
+# Verificar se o script está sendo executado como root
+if (( $EUID != 0 )); then
+  echo "Por favor, execute como root"
+  exit
+fi
+
+# Atualiza o sistema
+sudo apt update && sudo apt upgrade -y
+
+# Instalar o Docker e o Docker Compose se eles não estiverem instalados
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Docker não está instalado. Instalando...'
+  sudo apt-get install docker.io -y
+fi
+
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Docker Compose não está instalado. Instalando...'
+  sudo apt-get install docker-compose -y
+fi
+
+# Instalar o Git se ele não estiver instalado
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Git não está instalado. Instalando...'
+  sudo apt-get install git -y
+fi
 
 # Clonar o repositório do GitHub
 git clone $repo_url
